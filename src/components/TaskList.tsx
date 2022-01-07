a5fc0a821b48d3d1598ea7ab689dc32421ead61f
Fernanda Querino <fequerinodelima@gmail.com>
2022-01-07 01:29:03 -0300

    add: funcionalidade de remover task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -56,6 +56,10 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const newArrayofTasks = tasks.filter(task => task.id != id);
+
+    setTasks(newArrayofTasks);
   }
 
   return (
